9826804d049094d1e9692cbcdd09981e8608e01e
ALEJANDRO DUARTE LOBO <alejandro.duarte@ucr.ac.cr>
2020-06-11 10:40:34 -0600

    NF

--- NF/NF.py
+++ NF/NF.py
@@ -1,4 +1,4 @@
-def nextfit(vms,server_capacity):
+def nextfit(vms,server_capacity, bins):
     servers=0
     rem=server_capacity
     for x in range(len(vms)):
@@ -6,10 +6,15 @@ def nextfit(vms,server_capacity):
             rem=rem-vms[x]
         else:
             servers+=1
+            bins.append(server_capacity-rem)
             rem=server_capacity-vms[x]
     return servers
 
 vms=[2,5,4,7,1,3,8]
 server_capacity=10
-
-print("se se necesitan ",nextfit(vms,server_capacity)," Servidores de capacidad ",server_capacity, "para las ", len(vms), "maquinas virtuales especificadas")
+bins=[]
+print("se se necesitan ",nextfit(vms,server_capacity,bins)," Servidores de capacidad ",server_capacity, "para las ", len(vms), "maquinas virtuales especificadas")
+print("VMS:",vms)
+print("Bins:")
+for x in range(len(bins)):
+    print("Server",x+1,"Espacio Usado:", bins[x])
