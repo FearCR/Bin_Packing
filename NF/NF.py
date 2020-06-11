0b1c64ab50a2668328dd193a1c775c40674ca942
ALEJANDRO DUARTE LOBO <alejandro.duarte@ucr.ac.cr>
2020-06-11 10:56:55 -0600

    random generated and sorted

--- NF/NF.py
+++ NF/NF.py
@@ -1,4 +1,7 @@
+import time
+from random import randrange
 def nextfit(vms,server_capacity, bins):
+    vms.sort(reverse=True)
     servers=0
     rem=server_capacity
     for x in range(len(vms)):
@@ -9,12 +12,23 @@ def nextfit(vms,server_capacity, bins):
             bins.append(server_capacity-rem)
             rem=server_capacity-vms[x]
     return servers
-
-vms=[2,5,4,7,1,3,8]
+vms_ammount=1000
+vms=[]
 server_capacity=10
 bins=[]
-print("se se necesitan ",nextfit(vms,server_capacity,bins)," Servidores de capacidad ",server_capacity, "para las ", len(vms), "maquinas virtuales especificadas")
+
+for x in range(vms_ammount):
+    vms.append(randrange(1,server_capacity))
+
+
+start_time=time.time()
+server_ammount=nextfit(vms,server_capacity,bins)
+elapsed_time=time.time()-start_time
+
 print("VMS:",vms)
 print("Bins:")
 for x in range(len(bins)):
     print("Server",x+1,"Espacio Usado:", bins[x])
+
+print("se se necesitan ",server_ammount," Servidores de capacidad ",server_capacity, "para las ", len(vms), "maquinas virtuales especificadas")
+print("tiempo en que se encontro esta solucion optima=",elapsed_time)
