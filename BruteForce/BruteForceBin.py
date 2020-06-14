17abdff4250ec86a7fc49afd14888b2d0c150466
ALEJANDRO DUARTE LOBO <alejandro.duarte@ucr.ac.cr>
2020-06-14 16:14:31 -0600

    Brute force permutations

--- BruteForce/BruteForceBin.py
+++ BruteForce/BruteForceBin.py
@@ -1,3 +1,4 @@
+import time
 def perms(elements):
     if len(elements) <=1:
         yield elements
@@ -8,9 +9,8 @@ def perms(elements):
 
 
 
-def VM_allocate(vms, server_capacity):
+def VM_allocate(vms, server_capacity):#next fit
     servers=1
-    print(vms)
     rem=server_capacity
     for i in range(len(vms)):
         if rem>=vms[i]:
@@ -23,15 +23,17 @@ def VM_allocate(vms, server_capacity):
 
 
 if __name__ == '__main__':
-    VM_list=(4, 8, 1, 4, 2, 1)
+    VM_list=(4,8,1,4,2,1,9,9,1,1)
     server_capacity=10
     min_bins=999999999
     order=[]
+    t0=time.time()
     for i in perms(VM_list):
         temp=VM_allocate(i,server_capacity)
-        print(temp)
         if min_bins>temp:
             min_bins=temp
             order=i
+    elapsed=time.time()-t0
     print ("se necesita un minimo de ", min_bins,"servidores de capacidad",server_capacity,"en el orden",order)
+    print ("Esta solucion optima se encontro en",elapsed,"segundos")
     exit()
